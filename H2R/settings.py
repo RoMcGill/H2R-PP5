89570671c15b72335150e962fd83605d7c7a2573
RoMcGill <ro.mcgill12@gmail.com>
2022-09-07 12:58:32 +0000

    add static folder + files, mockup index.html+navbar

--- H2R/settings.py
+++ H2R/settings.py
@@ -153,3 +153,14 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+os.path.join(BASE_DIR, 'static'),
+)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Media Files
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
