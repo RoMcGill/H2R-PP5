fa869f15e2ef7645920b04dec4d8ee734d6e4516
RoMcGill <ro.mcgill12@gmail.com>
2022-09-08 19:31:41 +0000

    add view for brand detail

--- brands/views.py
+++ brands/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Brands
 
 # Create your views here.
@@ -15,3 +15,16 @@ def brands(request):
 
     }
     return render(request, 'brands.html', context)
+
+
+    def brand_detail(request, brand_id):
+    """
+    view to return brand details page
+    """
+    brand = get_object_or_404(brand, pk=brand_id)
+
+    context = {
+        'brand': brand,
+
+    }
+    return render(request, 'brands-detail.html', context)
