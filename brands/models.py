f6199e00f8a0d51743dae5e86b845cab85f60c39
RoMcGill <ro.mcgill12@gmail.com>
2022-09-08 16:30:33 +0000

    install pillow+ migrate

--- brands/models.py
+++ brands/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Brands(models.Model):
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
