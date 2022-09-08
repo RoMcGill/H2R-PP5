6fd79653ea68c03e54a0007630ee8eb2fe5e4a17
RoMcGill <ro.mcgill12@gmail.com>
2022-09-08 15:47:16 +0000

    add templates to brands and style brand.html+ add pictures

--- brands/views.py
+++ brands/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def brands(request):
+    """
+    view to return index page
+    """
+    return render(request, 'brands.html')
