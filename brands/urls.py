a1745f4e0c8b2b425e0695bcd9f0a4983b2ada01
RoMcGill <ro.mcgill12@gmail.com>
2022-09-14 09:42:06 +0000

    add link to upload brand to index.html try add product detail

--- brands/urls.py
+++ brands/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
 
        path('', views.brands, name='brands'),
        path('<brand_id>', views.brand_detail, name='brand_detail'),
-       path('<brand_product_id>', views.brand_detail, name='brand_product')
+       path('<brand_product_id>', views.brand_detail, name='brand_product'),
+       # path('product_detail', views.product_detail, name='product_detail'),
 ]
