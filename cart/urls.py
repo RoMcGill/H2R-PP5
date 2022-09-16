cea0d31fb0ad37c1cb403c60079503beb9f1529e
RoMcGill <ro.mcgill12@gmail.com>
2022-09-16 15:09:45 +0000

    add cart template content+ style

---
+++ cart/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+
+       path('', views.view_cart, name='view_cart')
+]
--- cart/views.py
+++ cart/views.py
@@ -5,6 +5,6 @@ from django.shortcuts import render
 
 def view_cart(request):
     """
-    view to return index page
+    view to return cart contents page
     """
     return render(request, 'cart/cart.html')
