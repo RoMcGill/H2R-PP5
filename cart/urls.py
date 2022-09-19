240aec4b7e760921ae52d8c8b29875494005bb8a
RoMcGill <ro.mcgill12@gmail.com>
2022-09-19 11:46:36 +0000

    add add to car functionalitu

--- cart/urls.py
+++ cart/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
 
-       path('', views.view_cart, name='view_cart')
+       path('', views.view_cart, name='view_cart'),
+       path('add/<item_id>/', views.add_to_cart, name='add_to_cart')
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -8,3 +8,20 @@ def view_cart(request):
     view to return cart contents page
     """
     return render(request, 'cart/cart.html')
+
+
+def add_to_cart(request, item_id):
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    cart = request.session.get('cart',{})
+
+    if item_id in list(cart.keys()):
+        cart[item_id] += quantity
+    else:
+
+        cart[item_id] = quantity
+    request.session['cart'] = cart
+    print(request.session['cart'])
+    return redirect(redirect_url)
+
+
