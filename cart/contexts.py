744179d2d67acfed60df3cbc00120e00a43d01a1
RoMcGill <ro.mcgill12@gmail.com>
2022-09-21 15:54:19 +0000

    add to context processor to show items in cart+ add if statement to show total under cart on navbar

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,11 +1,25 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from brands.models import Brand_products
 
 def cart_contents(request):
 
     cart_items = []
     total = 0
     product_count = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, quantity in cart.items():
+        product = get_object_or_404(Brand_products, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
+
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE)
