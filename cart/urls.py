911b45c417f026c70d0e5dcffb5c5f26741bd82b
RoMcGill <ro.mcgill12@gmail.com>
2022-10-06 19:21:01 +0000

    adding commit to test changes

--- cart/urls.py
+++ cart/urls.py
@@ -1,9 +1,11 @@
-from django.contrib import admin
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
 
        path('', views.view_cart, name='view_cart'),
-       path('add/<item_id>/', views.add_to_cart, name='add_to_cart')
+       path('add/<item_id>/', views.add_to_cart, name='add_to_cart'),
+       path('adjust/<item_id>/', views.adjust_cart, name='adjust_cart'),
+
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+from brands.models import Brand_products
 
 # Create your views here.
 
@@ -23,5 +24,37 @@ def add_to_cart(request, item_id):
     request.session['cart'] = cart
 
     return redirect(redirect_url)
+
+
+def adjust_cart(request, item_id):
+    product = get_object_or_404(Brand_products, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    cart = request.session.get('cart', {})
+    print(quantity)
+    if quantity > 0:
+        cart[item_id] = quantity
+
+    else:
+        cart.pop(item_id)
+        print("item pop")
+
+    request.session['cart'] = cart
+    print("redirected")
+    return redirect(reverse('view_cart'))
+
+
+# def remove_from_cart(request, item_id):
+
+#     cart = request.session.get('cart', {})
+#     cart.pop(item_id)
+#     print("item pop")
+
+#     request.session['cart'] = cart
+#     print("redirected")
+#     return HttpResponse(status=200)
+
+
+
+
 
 
