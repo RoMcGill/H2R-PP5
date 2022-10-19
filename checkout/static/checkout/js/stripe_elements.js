5fd309217c89d5a7dcad9d47fca9d7d389e0bc2d
RoMcGill <ro.mcgill12@gmail.com>
2022-10-19 15:57:08 +0000

    add payment input area from stripe

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -4,3 +4,14 @@ var stripe = Stripe(stripe_public_key);
 var elements = stripe.elements();
 var card = elements.create('card');
 card.mount("#card-element");
+
+// Set up Stripe.js and Elements to use in checkout form
+var elements = stripe.elements();
+var style = {
+  base: {
+    color: "#32325d",
+  }
+};
+
+var card = elements.create("card", { style: style });
+card.mount("#card-element");
