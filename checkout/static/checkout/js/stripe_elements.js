2a2e7e20a0025a28d383fba4ab3a4c2daf26b4bf
RoMcGill <ro.mcgill12@gmail.com>
2022-10-21 16:30:02 +0000

    add cach checout data function to get data from webhook, bug with completing order form now

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -41,12 +41,45 @@ form.addEventListener('submit', function(ev) {
   $('#submit-button').attr('disabled',true);
   $('#payment-form').fadeToggle(100);
   $('#loading-overlay').fadeToggle(100);
-  // If the client secret was rendered server-side as a data-secret attribute
-  // on the <form> element, you can retrieve it here by calling `form.dataset.secret`
-  console.log(clientSecret)
+
+var saveInfo = Boolean($('#id-save-info').attr('checked'));
+var csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
+var postData = {
+  'csrfmiddlewaretoken': csrfToken,
+  'client_secret': clientSecret,
+  'save_info': saveInfo,
+}
+var url = '/checkout/cache_checkout_data/';
+
+$.post(url, postData).done(function(){
   stripe.confirmCardPayment(clientSecret, {
     payment_method: {
       card: card,
+      billing_details: {
+        name: $.trim(form.full_name.value),
+        phone: $.trim(form.phone_number.value),
+        email: $.trim(form.email.value),
+        address:{
+          line1: $.trim(form.street_address1.value),
+          line2: $.trim(form.street_address2.value),
+          city: $.trim(form.town_or_city.value),
+          country: $.trim(form.country.value),
+          state: $.trim(form.county.value),
+        }
+      }
+    },
+    shipping: {
+      name: $.trim(form.full_name.value),
+      phone: $.trim(form.phone_number.value),
+
+      address:{
+        line1: $.trim(form.street_address1.value),
+        line2: $.trim(form.street_address2.value),
+        city: $.trim(form.town_or_city.value),
+        country: $.trim(form.country.value),
+        postal_code: $.trim(form.postcode.value),
+        state: $.trim(form.county.value),
+      }
     }
   }).then(function(result) {
     if (result.error) {
@@ -63,4 +96,8 @@ form.addEventListener('submit', function(ev) {
       }
     }
   });
+}).fail(function () {
+  // just reload the page, the error will be in django messages
+  location.reload();
+})
 });
--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -13,6 +13,68 @@ class StripeWH_Handler:
             status=200)
 
     def handle_payment_intent_succeeded(self, event):
+        intent = event.data.object
+        pid = intent.item_data
+        cart = intent.metadata.cart
+        save_info = intent.metadata.save_info
+
+        billing_details = intent.charges.data[0].billing_details
+        shipping_details = intent.shipping_details
+        grand_total = round(intent.data.charges[0].amount/100, 2)
+
+        for field, value in shipping_details.address.items():
+            if value == "":
+                shipping_details.address[field] = None
+
+        order_exists = False
+        try:
+
+            order = Order.objects.get(
+                full_name__iexact=shipping_details.name,
+                email__iexact=shipping_details.email,
+                phone_number__iexact=shipping_details.phone,
+                country__iexact=shipping_details.country,
+                postcode__iexact=shipping_details.postal_code,
+                town_or_city__iexact=shipping_details.city,
+                street_address1__iexact=shipping_details.line1,
+                street_address2__iexact=shipping_details.line2,
+                county__iexact=shipping_details.state,
+            )
+            order_exists = True
+            return HttpResponse(
+                content=f'Webhook recieved: {event["type"]} | SUCCESS verified order already in database',
+                status=200)
+        except Order.DoesNotExist:
+            try:
+
+                order = Order.objects.create(
+                    full_namect=shipping_details.name,
+                    emailct=shipping_details.email,
+                    phone_numberct=shipping_details.phone,
+                    countryct=shipping_details.country,
+                    postcodect=shipping_details.postal_code,
+                    town_or_cityct=shipping_details.city,
+                    street_address1ct=shipping_details.line1,
+                    street_address2ct=shipping_details.line2,
+                    county__=shipping_details.state,
+                )
+
+                for item_id, item_data in json.loads(cart)():
+                    product = Brand_products.objects.get(id=item_id)
+                    if isinstance(item_data, int):
+                        order_line_item = OrderLineItem(
+                            order=order,
+                            product=product,
+                            quantity=item_data,
+                        )
+                        order_line_item.save()
+            except Exception as e:
+                if order:
+                    order.delete()
+                return HttpResponse(
+                    content=f'Webhook recieved: {event["type"]} | ERROR: {e}',
+                    status=500)
+
 
         return HttpResponse(
             content=f'Webhook recieved: {event["type"]}',
