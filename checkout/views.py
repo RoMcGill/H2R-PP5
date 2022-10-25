3f75541ea96484f67590ed8ab330b364c96542b3
RoMcGill <ro.mcgill12@gmail.com>
2022-10-25 19:00:41 +0000

    linked orders to user profiles

--- checkout/views.py
+++ checkout/views.py
@@ -6,6 +6,8 @@ from .forms import OrderForm
 from brands.models import Brand_products
 from .models import Order, OrderLineItem
 from cart.contexts import cart_contents
+from profiles.forms import UserProfileForm
+from profiles.models import UserProfile
 
 import stripe
 import json
@@ -109,6 +111,28 @@ def checkout(request):
 def checkout_success(request, order_number):
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
+
+    if request.user.is_authenticated:
+        profile = UserProfile.objects.get(user=request.user)
+        order.user_profile = profile
+        order.save()
+
+        if save_info:
+            profile_data = {
+             'default_phone_number': order.phone_number,
+             'default_country': ordercCountry,
+             'default_postcode': order.postcode,
+             'default_town_or_city': order.town_or_city,
+             'default_street_address1': order.street_address_1,
+             'default_street_address2': order.street_address_2,
+             'default_county': order.county,
+            }
+            user_profile_form = UserProfileForm(profile_data, instance=profile)
+            if user_profile_form.is_valid:
+                user_profile_form.save()
+
+
+
     messages.success(request, f'Order processed \
         your order number is {order_number} . A confirmation \
         email will be sent to {order.email} shortly.')
--- profiles/views.py
+++ profiles/views.py
@@ -4,6 +4,8 @@ from django.contrib import messages
 from .models import UserProfile
 from .forms import UserProfileForm
 
+from checkout.models import Order
+
 
 def profile(request):
     """ Display the user's profile. """
@@ -27,3 +29,21 @@ def profile(request):
     }
 
     return render(request, template, context)
+
+def order_history(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request,(
+        f'this is a past conformation for {order_number}. '
+        'A confirmation email was sent on the order date'
+    ))
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+        'from_profile': True,
+
+    }
+
+    return render(request, template, context)
+
