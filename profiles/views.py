615b6ef872d4a10ca25ef8dd4066ca6d3e97ed69
RoMcGill <ro.mcgill12@gmail.com>
2022-10-25 18:13:31 +0000

    add profile details and order history to profile page

--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import UserProfile
+from .forms import UserProfileForm
 
 
 def profile(request):
     """ Display the user's profile. """
+    profile = get_object_or_404(UserProfile, user=request.user)
+    orders = profile.orders.all()
+
+    form = UserProfileForm(instance=profile)
 
     template = 'profiles/profiles.html'
-    context = {}
+    context = {
+        'form': form,
+        'orders': orders,
+    }
 
     return render(request, template, context)
