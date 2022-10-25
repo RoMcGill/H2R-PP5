6e5766f694d2f4e29e4266b96491b4b27c6bb282
RoMcGill <ro.mcgill12@gmail.com>
2022-10-25 18:22:54 +0000

    fix success toast showing cart + update form in profile

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 
 from .models import UserProfile
 from .forms import UserProfileForm
@@ -7,6 +8,13 @@ from .forms import UserProfileForm
 def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'profile updated succesfully')
+
     orders = profile.orders.all()
 
     form = UserProfileForm(instance=profile)
@@ -15,6 +23,7 @@ def profile(request):
     context = {
         'form': form,
         'orders': orders,
+        'on_profile_page': True
     }
 
     return render(request, template, context)
