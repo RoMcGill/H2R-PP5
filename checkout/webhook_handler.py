a8a020d8d4d255d265bc53b12843bfea1261e791
RoMcGill <ro.mcgill12@gmail.com>
2022-10-26 17:04:37 +0000

    merged commits from github to repo

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -38,7 +38,7 @@ class StripeWH_Handler:
         if username != 'AnonymousUser':
             profile = UserProfile.objects.get(user__username=username)
             if save_info:
-                profile.default_phone_number=shipping_details.phone
+                profile.default_phone_number=shipping_details.phonegit push
                 profile.default_country=shipping_details.address.country
                 profile.default_postcode=shipping_details.address.postal_code
                 profile.default_town_or_city=shipping_details.address.city
