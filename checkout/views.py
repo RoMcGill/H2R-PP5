641f6be19a97d19b2c6cd4e3fdc0fccac9fd3e9b
RoMcGill <ro.mcgill12@gmail.com>
2022-10-26 17:01:15 +0000

    webhook now handles user profiles

--- checkout/views.py
+++ checkout/views.py
@@ -91,8 +91,24 @@ def checkout(request):
             amount=stripe_total,
             currency=settings.STRIPE_CURRENCY,
         )
-
-    order_form = OrderForm()
+    if request.user.is_authenticated:
+        try:
+            profile = UserProfile.objects.get(user=request.user)
+            order_form = OrderForm(initial={
+                'full_name': profile.user.get_full_name(),
+                'email': profile.user.email,
+                'phone_number': profile.default_phone_number,
+                'country': profile.default_country,
+                'postcode': profile.default_postcode,
+                'town_or_city': profile.default_town_or_city,
+                'street_address1': profile.default_street_address1,
+                'street_address2': profile.default_street_address2,
+                'county': profile.default_county,
+            })
+        except UserProfile.DoesNotExist:
+            order_form = OrderForm()
+    else:
+        order_form = OrderForm()
 
     if not stripe_public_key:
         messages.warning(request, 'stripe public key not set in environment')
--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from .models import Order, OrderLineItem
 from brands.models import Brand_products
+from profiles.models import UserProfile
 
 import json
 import time
@@ -30,6 +31,22 @@ class StripeWH_Handler:
         for field, value in shipping_details.address.items():
             if value == "":
                 shipping_details.address[field] = None
+
+        # check box for user info
+        profile = None
+        username = intent.metadata.username
+        if username != 'AnonymousUser':
+            profile = UserProfile.objects.get(user__username=username)
+            if save_info:
+                profile.default_phone_number=shipping_details.phone
+                profile.default_country=shipping_details.address.country
+                profile.default_postcode=shipping_details.address.postal_code
+                profile.default_town_or_city=shipping_details.address.city
+                profile.default_street_address1=shipping_details.address.line1
+                profile.default_street_address2=shipping_details.address.line2
+                profile.default_county=shipping_details.address.state
+                profile.save()
+
 
         order_exists = False
         attempt =1
@@ -67,6 +84,7 @@ class StripeWH_Handler:
 
                 order = Order.objects.create(
                     full_name=shipping_details.name,
+                    user_profile=profile,
                     email=billing_details.email,
                     phone_number=shipping_details.phone,
                     country=shipping_details.address.country,
