9e2a70c8021c4dfa4a0140645c1889f3519a71fb
RoMcGill <ro.mcgill12@gmail.com>
2022-11-01 15:05:18 +0000

    setup  postgres

--- H2R/settings.py
+++ H2R/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,6 +133,9 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+
 
 
 # Password validation
