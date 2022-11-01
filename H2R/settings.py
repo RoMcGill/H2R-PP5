a7c100c73ec26d1d99ca32b33a63aa4e6055fcb5
RoMcGill <ro.mcgill12@gmail.com>
2022-11-01 15:50:17 +0000

    remove secret key and set debug

--- H2R/settings.py
+++ H2R/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i@evzfa64yxe8&lk*w=9z6p(qavf&xg0^f^28de_!so)v0y%5&'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['h2r-pp5.herokuapp.com', 'localhost', '*']
 
