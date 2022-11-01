50ab8237ef5ce5fcc06041fb6bfc75496fa477d6
RoMcGill <ro.mcgill12@gmail.com>
2022-11-01 17:22:19 +0000

    fix syntax error causing deployment to fail

--- H2R/settings.py
+++ H2R/settings.py
@@ -205,7 +205,7 @@ if 'USE_AWS' in os.environ:
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
-    STATICFILES_STORAGE = 'custom_storage.StaticStorage'
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
     STATICFILES_LOCATION = 'static'
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
