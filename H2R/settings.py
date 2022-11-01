51f444d2b6814fb3390edf11c872b1235b314db4
RoMcGill <ro.mcgill12@gmail.com>
2022-11-01 17:29:40 +0000

    add casche control

--- H2R/settings.py
+++ H2R/settings.py
@@ -199,6 +199,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 if 'USE_AWS' in os.environ:
+
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000'
+    }
+
     AWS_STORAGE_BUCKET_NAME = 'h2r-pp5'
     AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
