5be94bcbb5ec7c40c079c96cf24f2c07c02f4608
RoMcGill <ro.mcgill12@gmail.com>
2022-11-02 12:30:54 +0000

    changed email 'smpt' to smtp  in settings.py

--- H2R/settings.py
+++ H2R/settings.py
@@ -233,10 +233,10 @@ if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'info.home2retail@gmail.com'
 else:
-    EMAIL_BACKEND = 'django.core.mail.backends.smpt.EmailBackend'
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
-    EMAIL_HOST = 'smpt.gmail.com'
+    EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
