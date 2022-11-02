4cd3efe3c3feb220726dc66fd6bd4b347b4f89ed
RoMcGill <ro.mcgill12@gmail.com>
2022-11-02 12:02:03 +0000

    change debug to true to debug

--- H2R/settings.py
+++ H2R/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+# DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['h2r-pp5.herokuapp.com', 'localhost', '*']
 
