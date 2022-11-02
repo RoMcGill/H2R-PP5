9bead9d2644ba6aa40300fa6144cf3784116770c
RoMcGill <ro.mcgill12@gmail.com>
2022-11-02 12:29:36 +0000

    changed email 'post' to email PORT in settings.py

--- H2R/settings.py
+++ H2R/settings.py
@@ -235,9 +235,9 @@ if 'DEVELOPMENT' in os.environ:
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smpt.EmailBackend'
     EMAIL_USE_TLS = True
-    EMAIL_POST = 587
+    EMAIL_PORT = 587
     EMAIL_HOST = 'smpt.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-    EMAIL_HOST_PASS = os.environ.get('EMAIL_HOST_PASS')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
