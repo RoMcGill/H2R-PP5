b5e6bcb216e9184ab2395fae331be46a425c1f1e
RoMcGill <ro.mcgill12@gmail.com>
2022-11-16 19:28:25 +0000

    removed conflicting styling from checkout/base.css

--- H2R/settings.py
+++ H2R/settings.py
@@ -230,6 +230,7 @@ STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
+
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'info.home2retail@gmail.com'
