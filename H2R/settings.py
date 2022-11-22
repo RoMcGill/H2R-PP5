b30e791d2878708d4edef6b5bea58800a5990217
RoMcGill <ro.mcgill12@gmail.com>
2022-11-22 19:33:56 +0000

    changed settings to Test deployed site for submission

--- H2R/settings.py
+++ H2R/settings.py
@@ -22,16 +22,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-if 'DEVELOPMENT' in os.environ:
-    SECRET_KEY = os.environ.get('SECRET_KEY', '')
-else:
-    SECRET_KEY = '!g@9phuj3y@^(&aw45%$25()mjfd-)tzgz8p9h)0)+r#)6l5o9'
+
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
+
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['h2r-pp5.herokuapp.com', 'localhost', '*']
+ALLOWED_HOSTS = ['h2r-pp5.herokuapp.com', 'localhost', ]
 
 
 # Application definition
@@ -233,14 +233,14 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 
 # if 'DEVELOPMENT' in os.environ:
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 DEFAULT_FROM_EMAIL = 'info.home2retail@gmail.com'
 # else:
-#     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-#     EMAIL_USE_TLS = True
-#     EMAIL_PORT = 587
-#     EMAIL_HOST = 'smtp.gmail.com'
-#     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-#     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
-#     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
